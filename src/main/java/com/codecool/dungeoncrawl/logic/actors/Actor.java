1221b9d8ffe168f6849aef83ff7464bf3433997c
NataliaGol <nataliahreast@gmail.com>
2022-11-08 22:32:28 +0100

    Don't walk into walls or enemies

--- src/main/java/com/codecool/dungeoncrawl/logic/actors/Actor.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/actors/Actor.java
@@ -1,22 +1,30 @@
 package com.codecool.dungeoncrawl.logic.actors;
 
 import com.codecool.dungeoncrawl.logic.Cell;
+import com.codecool.dungeoncrawl.logic.CellType;
 import com.codecool.dungeoncrawl.logic.Drawable;
 
 public abstract class Actor implements Drawable {
-    private Cell cell;
-    private int health = 10;
+    protected Cell cell;
+    protected int health = 10;
 
-    public Actor(Cell cell) {
+    protected Actor(Cell cell) {
         this.cell = cell;
         this.cell.setActor(this);
     }
 
     public void move(int dx, int dy) {
         Cell nextCell = cell.getNeighbor(dx, dy);
+        if (isWallOrEnemy(nextCell)) {
+            return;
+        }
         cell.setActor(null);
         nextCell.setActor(this);
         cell = nextCell;
+    }
+
+    private static boolean isWallOrEnemy(Cell nextCell) {
+        return nextCell.getType().equals(CellType.WALL) || nextCell.getActor() != null;
     }
 
     public int getHealth() {
