422a7153179f3e98f530417ee25f4d61474a3a94
Duc Trung Bui <txbuiduc@gmail.com>
2022-11-23 22:16:25 +0100

    Connection update

--- src/main/java/com/codecool/dungeoncrawl/dao/GameDatabaseManager.java
+++ src/main/java/com/codecool/dungeoncrawl/dao/GameDatabaseManager.java
@@ -12,8 +12,8 @@ import java.sql.Statement;
 
 public class GameDatabaseManager {
     private PlayerDao playerDao;
-    Connection c = null;
-    Statement stmt = null;
+    private Connection c = null;
+    private Statement stmt = null;
 
     public void setup() throws SQLException {
         DataSource dataSource = connect();
@@ -49,16 +49,16 @@ public class GameDatabaseManager {
                     .getConnection("jdbc:postgresql://dungeon-crawl.postgres.database.azure.com:5432/postgres",
                             "ductrung2", "Dungeon2022?");
             System.out.println("Opened database successfully");
-            stmt = c.createStatement();
-            String sql = "CREATE TABLE GAMESTATE " +
-                    "(ID INT PRIMARY KEY     NOT NULL," +
-                    " MAP           INT     NOT NULL, " +
-                    " AGE           INT     NOT NULL, " +
-                    " HP            INT     NOT NULL, " +
-                    " STRENGTH      INT     NOT NULL)";
-            stmt.executeUpdate(sql);
-            stmt.close();
-            c.close();
+//            stmt = c.createStatement();
+//            String sql = "CREATE TABLE GAMESTATE " +
+//                    "(ID INT PRIMARY KEY     NOT NULL," +
+//                    " MAP           INT     NOT NULL, " +
+//                    " AGE           INT     NOT NULL, " +
+//                    " HP            INT     NOT NULL, " +
+//                    " STRENGTH      INT     NOT NULL)";
+//            stmt.executeUpdate(sql);
+//            stmt.close();
+//            c.close();
         } catch ( Exception e ) {
             System.err.println( e.getClass().getName()+": "+ e.getMessage() );
             System.exit(0);
